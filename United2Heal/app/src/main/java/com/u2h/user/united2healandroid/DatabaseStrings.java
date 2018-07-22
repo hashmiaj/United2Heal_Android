e0e2e2bc5d1f699f55f857407c0e81ff0bd72afe
Unknown <huggigames@googlemail.com>
2018-07-21 20:15:54 -0400

    Adding database export to csv

---
+++ United2Heal/app/src/main/java/com/u2h/user/united2healandroid/DatabaseStrings.java
@@ -0,0 +1,9 @@
+package com.u2h.user.united2healandroid;
+
+public class DatabaseStrings {
+    static final String DATABASE_URL="united2heal.cxsnwexuvrto.us-east-1.rds.amazonaws.com:3306";
+    static final String DATABASE_NAME="u2hdb";
+    static final String USERNAME="united2heal";
+    static final String PASSWORD="ilovevcu123";
+
+}
