0d1cf05b14381246048b4fa16e02cf458c5ff6b0
Unknown <huggigames@googlemail.com>
2018-07-31 18:41:49 -0400

    Connecting Add item to Database

--- United2Heal/app/src/main/java/com/u2h/user/united2healandroid/AddItemPage.java
+++ United2Heal/app/src/main/java/com/u2h/user/united2healandroid/AddItemPage.java
@@ -1,5 +1,6 @@
 package com.u2h.user.united2healandroid;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,8 +8,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class AddItemPage extends Fragment {
+    ArrayList<String> categoryList=new ArrayList<>();
+    ArrayList<Item> itemArrayList=new ArrayList<>();
+    Spinner itemCategorySpinner;
+    Button generateCodeButton;
+    TextView codeInputTextView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -18,5 +38,66 @@ public class AddItemPage extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        itemCategorySpinner=(Spinner)view.findViewById(R.id.itemCategorySpinner);
+        codeInputTextView=(TextView) view.findViewById(R.id.itemCodeInput);
+        generateCodeButton=(Button) view.findViewById(R.id.generateCodeButton);
+        generateCodeButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int rand= (int)(Math.random()*100000);
+                codeInputTextView.setText(String.valueOf(rand));
+            }
+        });
+        GetData data= new GetData();
+        data.execute();
+    }
+    private class GetData extends AsyncTask<String,String,String>
+    {
+        Connection conn= null;
+        Statement stmnt=null;
+
+        @Override
+        protected void onPreExecute(){
+            categoryList.clear();
+            itemArrayList.clear();
+        }
+        @Override
+        protected String doInBackground(String... strings) {
+            try{
+                Class.forName(DatabaseStrings.JDBC_DRIVER);
+                conn= DriverManager.getConnection(DatabaseStrings.DB_URL,DatabaseStrings.USERNAME,DatabaseStrings.PASSWORD);
+                stmnt=conn.createStatement();
+                String sql="Select * from u2hdb.ItemTable";
+                ResultSet rs=stmnt.executeQuery(sql);
+                while (rs.next())
+                {
+                    categoryList.add(rs.getString("CategoryName"));
+                    Item item= new Item(rs.getString("CategoryName"),rs.getInt("ItemID"),rs.getString("ItemName"));
+                    itemArrayList.add(item);
+                }
+                ArrayList<String> temp= new ArrayList<>();
+                HashSet<String> hashSet=new HashSet<>();
+                for(String s:categoryList)
+                {
+                    if(!hashSet.contains(s))
+                    {
+                        temp.add(s);
+                        hashSet.add(s);
+                    }
+                }
+                categoryList=temp;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String msg)
+        {
+            ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(getActivity(),R.layout.spinner_item_small,categoryList);
+            itemCategorySpinner.setAdapter(arrayAdapter);
+        }
     }
 }
--- United2Heal/app/src/main/java/com/u2h/user/united2healandroid/ItemPage.java
+++ United2Heal/app/src/main/java/com/u2h/user/united2healandroid/ItemPage.java
@@ -3,10 +3,15 @@ package com.u2h.user.united2healandroid;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -21,7 +26,12 @@ public class ItemPage extends AppCompatActivity {
     TextView itemIdTextView;
     String selectedItem;
     Spinner itemBoxSpinner;
-    ArrayList<String> boxList= new ArrayList<>();
+    Button submitButton;
+    String itemQuantity;
+    String selectedBox;
+    Boolean connectedToDB=false;
+    ArrayList<String> boxList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +40,38 @@ public class ItemPage extends AppCompatActivity {
         itemCategoryTextView = (TextView) findViewById(R.id.itemCategoryTextView);
         itemIdTextView = (TextView) findViewById(R.id.codeTextView);
         itemBoxSpinner = (Spinner) findViewById(R.id.itemBoxSpinner);
+        submitButton = (Button) findViewById(R.id.submitButton);
+        final TextView quantityTextView = (TextView) findViewById(R.id.quantityEdit);
+        quantityTextView.setText("");
+        itemBoxSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedBox = itemBoxSpinner.getAdapter().getItem(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        submitButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                itemQuantity = quantityTextView.getText().toString();
+                if(itemQuantity.equals("")) {
+                    Toast.makeText(getApplicationContext(),"Error: Enter a quantity ",Toast.LENGTH_SHORT).show();
+                }
+                else if(!connectedToDB)
+                {
+                    Toast.makeText(getApplicationContext(),"Wait for Database to connect",Toast.LENGTH_SHORT).show();
+
+                }
+                else  {
+                    PostData postData = new PostData();
+                    postData.execute();
+                }
+            }
+        });
         if (getIntent().getExtras() != null) {
             selectedItem = getIntent().getExtras().getString("com.u2h.user.united2healandroid.ITEM_NAME");
             itemNameTextView.setText(selectedItem);
@@ -46,7 +88,7 @@ public class ItemPage extends AppCompatActivity {
 
         @Override
         protected void onPreExecute() {
-
+            boxList.clear();
         }
 
         @Override
@@ -62,10 +104,9 @@ public class ItemPage extends AppCompatActivity {
                     itemCategory = rs.getString("CategoryName");
                     itemID = rs.getInt("ItemID") + "";
                 }
-                sql="Select * from u2hdb.BoxTable WHERE IsOpen=1 and CategoryName='"+itemCategory+"'";
-                rs=stmnt.executeQuery(sql);
-                while (rs.next())
-                {
+                sql = "Select * from u2hdb.BoxTable WHERE IsOpen=1 and CategoryName='" + itemCategory + "'";
+                rs = stmnt.executeQuery(sql);
+                while (rs.next()) {
                     boxList.add(rs.getString("BoxName"));
                 }
                 conn.close();
@@ -98,8 +139,79 @@ public class ItemPage extends AppCompatActivity {
         protected void onPostExecute(String msg) {
             itemCategoryTextView.setText(itemCategory);
             itemIdTextView.setText(itemID);
-            ArrayAdapter<String> dataAdapter=new ArrayAdapter<String>(ItemPage.this,R.layout.spinner_item,boxList);
+            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(ItemPage.this, R.layout.spinner_item, boxList);
             itemBoxSpinner.setAdapter(dataAdapter);
+            connectedToDB=true;
+        }
+    }
+
+    private class PostData extends AsyncTask<String, String, String> {
+        Connection conn;
+        Statement stmnt;
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                Class.forName(DatabaseStrings.JDBC_DRIVER);
+                conn = DriverManager.getConnection(DatabaseStrings.DB_URL, DatabaseStrings.USERNAME, DatabaseStrings.PASSWORD);
+                stmnt = conn.createStatement();
+
+                ArrayList<String> itemBoxEntries = new ArrayList<>();
+                ArrayList<Integer> itemBoxID = new ArrayList<>();
+                int random = (int) (Math.random() * 100000);
+                String sql = "Select Count(*) AS length from u2hdb.ItemBox where ItemName='" + selectedItem + "' AND BoxName='" + selectedBox + "'";
+                ResultSet rs = stmnt.executeQuery(sql);
+                rs.next();
+                int count=rs.getInt("length");
+                rs.close();
+                if(count>0)
+                {
+                    sql="UPDATE u2hdb.ItemBox SET ItemQuantity=ItemQuantity+"+itemQuantity+" where ItemName='" + selectedItem + "' AND BoxName='" + selectedBox + "'";
+                    stmnt.executeUpdate(sql);
+
+                }else {
+                    sql = "INSERT INTO u2hdb.ItemBox\n" +
+                            "Values (" + random + ",'','" + itemID + "','" + itemQuantity + "','" + selectedBox + "','" + selectedItem + "')";
+                    stmnt.executeUpdate(sql);
+                }
+                conn.close();
+                stmnt.close();
+            } catch (ClassNotFoundException e) {
+
+            } catch (SQLException e) {
+                if(e.getErrorCode()==23505)
+                {
+                    Toast.makeText(getApplicationContext(),"TRY AGAIN",Toast.LENGTH_SHORT).show();
+                }
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (conn != null)
+                        conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    if (stmnt != null)
+                        stmnt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String msg) {
+            Toast.makeText(getApplicationContext(),itemQuantity+" "+selectedItem+ " added to box "+selectedBox,Toast.LENGTH_SHORT).show();
+            finish();
+
         }
     }
 }
