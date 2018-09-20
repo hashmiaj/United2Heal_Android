ade0ed90ba75765b81ec1c9ddc269db84f667ac3
Unknown <huggigames@googlemail.com>
2018-09-20 17:52:59 -0400

    Adding expiration to export

--- United2Heal/app/src/main/java/com/u2h/user/united2healandroid/BoxStatsDataPoint.java
+++ United2Heal/app/src/main/java/com/u2h/user/united2healandroid/BoxStatsDataPoint.java
@@ -3,10 +3,11 @@ package com.u2h.user.united2healandroid;
 public class BoxStatsDataPoint {
     private String itemName;
     private int quantity;
-
-    public BoxStatsDataPoint(String itemName, int quantity) {
+    private  String expirationDate;
+    public BoxStatsDataPoint(String itemName, int quantity, String expirationDate) {
         this.itemName = itemName;
         this.quantity = quantity;
+        this.expirationDate=expirationDate;
     }
     public String getItemName() {
         return itemName;
@@ -22,5 +23,13 @@ public class BoxStatsDataPoint {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
     }
 }
